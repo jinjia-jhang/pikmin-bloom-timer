91b7e22283bebd5464429029d5c4f456f60681f7
Jin-Jia Jhang <50516663+jinjia-jhang@users.noreply.github.com>
2026-06-01 20:51:07 +0800

    hotfix 1

--- sw.js
+++ sw.js
@@ -1,28 +1,10 @@
-const CACHE_NAME = 'pikmin-timer-v6';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'pikmin-v7';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
-
-self.addEventListener('notificationclick', (e) => {
-  e.notification.close();
-  e.waitUntil(
-    clients.matchAll({ type: 'window' }).then(clients => {
-      if (clients.length > 0) return clients[0].focus();
-      return clients.openWindow('./');
-    })
-  );
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
