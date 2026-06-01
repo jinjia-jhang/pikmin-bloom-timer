12cb33a5983ffe9cff2409424dac3355326cfc50
Jin-Jia Jhang <50516663+jinjia-jhang@users.noreply.github.com>
2026-06-01 21:09:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,13 @@
-const CACHE_NAME = 'pikmin-v7';
+const CACHE_NAME = 'pikmin-v8';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    self.skipWaiting(); // 強制立即啟用
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
