e40530c2f20277887dca8d2ed69d5ea08ee43c65
Jin-Jia Jhang <50516663+jinjia-jhang@users.noreply.github.com>
2026-06-01 21:23:13 +0800

    hofix

--- sw.js
+++ sw.js
@@ -1,15 +1,10 @@
-const CACHE_NAME = 'pikmin-v8';
+const CACHE_NAME = 'pikmin-v10';
 const ASSETS = ['./', './index.html', './manifest.json'];
-
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-    self.skipWaiting(); // 強制立即啟用
+    self.skipWaiting();
 });
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(clients.claim());
-});
-
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', (e) => {
     e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
