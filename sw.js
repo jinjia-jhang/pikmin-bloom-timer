25dc5256ec81d6f6ca0f6e859a4911594802e75d
Jin-Jia Jhang <50516663+jinjia-jhang@users.noreply.github.com>
2026-06-10 00:38:19 +0800

    update v18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pikmin-v17';
+const CACHE_NAME = 'pikmin-v18';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
